2e651261bf649d565bd6812481e2e9b4151e8d58
Mathias <mroe734@aucklanduni.ac.nz>
2025-06-18 10:58:42 +1200

    Changed from clangd to harper

--- init.lua
+++ init.lua
@@ -691,7 +691,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {
+        harper_ls = {
           filetypes = { '.tpp', '.cpp', '.hpp' },
         },
 
@@ -778,7 +778,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, tpp = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
