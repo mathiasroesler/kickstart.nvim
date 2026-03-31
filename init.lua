3569b36e7c0a99de17537cb438e41bbc6eed8d71
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-03-31 14:32:40 +1300

    Changed theme to vague

--- init.lua
+++ init.lua
@@ -840,20 +840,18 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rose-pine/neovim',
+    'vague-theme/vague.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('rose-pine').setup {
+      require('vague').setup {
         styles = {
           comments = { italic = true }, -- Disable italics in comments
         },
       }
 
       -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'rose-pine-moon'
+      vim.cmd.colorscheme 'vague'
     end,
   },
 
