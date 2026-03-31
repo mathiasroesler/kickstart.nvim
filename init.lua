cbcbb32b967a5008eb098d915e7886549ce11ba6
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-03-31 13:36:49 +1300

    Changed theme to rose-pine-moon

--- init.lua
+++ init.lua
@@ -918,20 +918,20 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'rose-pine/neovim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('rose-pine').setup {
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = { italic = true }, -- Disable italics in comments
         },
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'duskfox'
+      vim.cmd.colorscheme 'rose-pine-moon'
     end,
   },
 
