527a18beb3bdd6425ad576f368b6a2a1c6ff0775
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-06-02 10:27:49 +1200

    Replaced noice with tiny-cmdline

--- init.lua
+++ init.lua
@@ -774,5 +774,13 @@ do
   }
   vim.cmd.colorscheme 'vague'
 
+  -- [[ Floating command line ]]
+  vim.o.cmdheight = 0
+  require("vim._core.ui2").enable({})
+  vim.pack.add{ "https://github.com/rachartier/tiny-cmdline.nvim"}
+  require("tiny-cmdline").setup({
+      on_reposition = require("tiny-cmdline").adapters.blink,
+  })
+
   pcall(require, 'custom.plugins')
 end
